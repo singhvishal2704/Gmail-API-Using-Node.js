dad7446b6161ddfaa5742296558e247a9cb7901f
Vishal Singh <singhvishal2704@gmail.com>
2021-08-08 19:59:36 +0530

    Initial Commit

--- api/controlllers/mailController.js
+++ api/controlllers/mailController.js
@@ -5,10 +5,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+/* Credentials has been stored in .env file */
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLEINT_SECRET = process.env.CLEINT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+/* Used OAuth 2.0 Google API for authentication */
+
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLEINT_SECRET,
@@ -16,10 +21,18 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
+
+// @desc    Get API Status
+// @route   GET /api/mail/
+// @access  Public
 const getMail = asyncHandler(async (req, res) => {
   res.send("API is running...");
 });
 
+
+// @desc    Mail Sender Function
+// @route   POST /api/mail/
+// @access  Public
 const mailSender = asyncHandler(async (req, res) => {
   async function sendMail() {
     try {
